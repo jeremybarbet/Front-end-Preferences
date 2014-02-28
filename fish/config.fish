08a41c208877f4ea1968f4e36f27267a0e6f90a1
Jérémy Barbet <jeremgraph@gmail.com>
2014-02-28 23:04:20 +0100

    Add default lines to fish config

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+# Remove greeting
+set fish_greeting ""
+
+# Java options
+setenv JAVA_OPTS "-Xms2048m -Xmx2048m -XX:MaxPermSize=2048m"
+setenv _JAVA_OPTS $JAVA_OPTS
+setenv _JAVA_OPTIONS $JAVA_OPTS
