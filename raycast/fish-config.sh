9aaa8245738b61ddfd377ae7eada964a8f9ef36b
Jérémy Barbet <jeremgraph@gmail.com>
2022-10-23 09:29:21 +0000

    Create fish-config.sh

---
+++ raycast/fish-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Fish Config
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ⚙️
+
+# Documentation:
+# @raycast.description Open configuration for fish shell
+
+code ~/.config/fish/config.fish
+
+echo "config.fish opened in vscode"
