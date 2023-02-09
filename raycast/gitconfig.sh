99a986317450a73e5585d72e9f8fb5f894515bc7
Jérémy Barbet <jeremgraph@gmail.com>
2023-02-09 11:03:50 +0000

    Create gitconfig.sh

---
+++ raycast/gitconfig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Gitconfig
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ⚙️
+
+# Documentation:
+# @raycast.description Open configuration for git
+
+code ~/.gitconfig
+
+echo ".gitconfig opened in vscode"
